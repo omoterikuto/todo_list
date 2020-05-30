72f0dfc5f5927c27b34996ac2577a0e6d2ff5808
omoterikuto <omoterikuto@gmail.com>
2020-05-30 14:44:21 +0900

    add: usertableとfolderの紐付け

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -1,9 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
-
 
 class FoldersTableSeeder extends Seeder
 {
@@ -14,11 +13,14 @@ class FoldersTableSeeder extends Seeder
    */
   public function run()
   {
+    $user = DB::table('users')->first();
+
     $titles = ['プライベート', '仕事', '旅行'];
 
     foreach ($titles as $title) {
       DB::table('folders')->insert([
         'title' => $title,
+        'user_id' => $user->id,
         'created_at' => Carbon::now(),
         'updated_at' => Carbon::now(),
       ]);
